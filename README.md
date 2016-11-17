ea779d6657f0e7f9d2866fb6dba05158e7e1c0eb
Stephane Gagnon <admin@pacificweb.ca>
2016-11-17 18:33:45 -0500

    Adjust try/catch

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # python-trafic-light-recorder
-A python script to record a trafic light for before switching to green
+A python script to record a trafic light for X seconds before switching to green and X seconds after 
+
+X=RecordDuration /2
--- recorder.py
+++ recorder.py
@@ -1,4 +1,5 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
 
 import sys
 import logging
@@ -11,74 +12,81 @@ from datetime import datetime
 def main(args=None):
 
 	#TODO ARGV 
-	loop=69
-	#loop=12
-	rectime=6
-	start=-(rectime/2)
+	loop=69					# Number of seconds for a complete Green to Gree cycle
+	rectime=6				# Number of seconds of recording
+	samples=10				# Number of video to capture
 
-	#try:
+	# Local variables
+	start=rectime/2			# Number of seconds for the startup before synchronize with loop
 
-	print "Camlight 1.0 / Ctrl+c to exit"
+	try:
 
-	# Camera
-	print "Warming up camera..."
-	camera = picamera.PiCamera()
-	time.sleep(2)
-	camera.resolution = (640,480)
-	camera.exposure_mode = "auto"
-	camera.image_effect = "none"
-	camera.exposure_compensation = 0
-	camera.ISO = 0
-	camera.brightness = 50
-	camera.contrast = 0
-	camera.framerate = 15
+		print "Ctrl+c to exit"
 
-	var = raw_input("Ready, press a key when light turn to GREEN # ")
+		# Camera
+		print "Warming up camera..."
+		camera = picamera.PiCamera()
+		time.sleep(2)
+		camera.resolution = (640,480)
+		camera.exposure_mode = "auto"
+		camera.image_effect = "none"
+		camera.exposure_compensation = 0
+		camera.ISO = 0
+		camera.brightness = 50
+		camera.contrast = 0
+		camera.framerate = 15
 
-	# Demarrage decalee par duree / 2 
-	time.sleep(loop+start)
-	timestr = time.strftime('%Y%m%d%H%M%S') + '.h264'
+		var = raw_input("Press a key when the trafic light turn to GREEN # ")
+
+		# Adjust for syncronization at startup for the first video
+		time.sleep(loop-start)
+		timestr = time.strftime('%Y%m%d%H%M%S') + '.h264'
       
-	# DEV
-	#print('start_recording ' + time.strftime('%Y%m%d%H%M%S'))
-	#time.sleep(rectime)
-			
-	# PROD
-	print('start_recording ' + timestr)
-	camera.start_recording(timestr)
-	print('wait_recording ' + str(rectime))
-	camera.wait_recording(rectime)
-	camera.stop_recording()
-	print('stop_recording ' + time.strftime('%Y%m%d%H%M%S'))
-			
-	# Loop ajustee
-	min=10
-	for i in range(2, min):
-
-		# Timeout before recording
-		print "Await timeout"
-		time.sleep(loop-rectime)
-
-		# DEV
-		#print('StartRecording ' + time.strftime('%Y%m%d%H%M%S'))
+		# Record Simulation
+		#print('start_recording ' + time.strftime('%Y%m%d%H%M%S'))
 		#time.sleep(rectime)
-
-		# PROD
-		timestr = time.strftime("%Y%m%d%H%M%S") + '.h264'
+			
+		# Live mode
 		print('start_recording ' + timestr)
 		camera.start_recording(timestr)
+
 		print('wait_recording ' + str(rectime))
 		camera.wait_recording(rectime)
-		print('stop_recording ' + time.strftime('%Y%m%d%H%M%S'))
-		camera.stop_recording()
 
-	#except (KeyboardInterrupt):
-	#	print('interrupt received')
-	#except :
-	#	print "Unexpected error:", sys.exc_info()[0]
-	#finally:
-	#	print('Exit finally')
-	#	sys.exit()
+		camera.stop_recording()
+		print('stop_recording ' + time.strftime('%Y%m%d%H%M%S'))
+			
+		# Next videos
+		for i in range(2, samples):
+
+			print "Awaiting light timeout"
+			time.sleep(loop-rectime)
+
+			# Simulation
+			#print('StartRecording ' + time.strftime('%Y%m%d%H%M%S'))
+			#time.sleep(rectime)
+
+			# PROD
+			timestr = time.strftime("%Y%m%d%H%M%S") + '.h264'
+
+			print('start_recording ' + timestr)
+			camera.start_recording(timestr)
+
+			print('wait_recording ' + str(rectime))
+			camera.wait_recording(rectime)
+
+			print('stop_recording ' + time.strftime('%Y%m%d%H%M%S'))
+			camera.stop_recording()
+
+	except (KeyboardInterrupt):
+		print('interrupt received')
+	except NameError, e:
+		print "NameError error:", e
+	except Exception, e: # catch *all* exceptions 
+		print "Unexpected error:", e
+	finally:
+		print('')
+		sys.exit()
 
 if __name__ == '__main__':
 	sys.exit(main(sys.argv[1:]) or 0)
